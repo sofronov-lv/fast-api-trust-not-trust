ca67ef0f9ec88d431b89dd102839f8d125b4fe08
ktdf <woolf.dr@mail.ru>
2024-02-03 15:20:01 +0300

    dev build

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import uvicorn
 import sys
 sys.path.append(".")
 
-from uvicorn.workers import UvicornWorker
-from gunicorn.app.base import BaseApplication
+# from uvicorn.workers import UvicornWorker
+# from gunicorn.app.base import BaseApplication
 
 from fastapi import FastAPI
 
@@ -18,22 +18,22 @@ from app.routes.search import router as search_router
 from app.routes.user import router as user_router
 
 
-class CustomGunicornApp(BaseApplication):
-    def __init__(self, app):
-        self.options = {
-            'bind': '0.0.0.0:8000',
-            'workers': 4,
-            'worker_class': 'uvicorn.workers.UvicornWorker'
-        }
-        self.application = app
-        super().__init__()
-
-    def load_config(self):
-        for key, value in self.options.items():
-            self.cfg.set(key, value)
-
-    def load(self):
-        return self.application
+# class CustomGunicornApp(BaseApplication):
+#     def __init__(self, app):
+#         self.options = {
+#             'bind': '0.0.0.0:8000',
+#             'workers': 4,
+#             'worker_class': 'uvicorn.workers.UvicornWorker'
+#         }
+#         self.application = app
+#         super().__init__()
+#
+#     def load_config(self):
+#         for key, value in self.options.items():
+#             self.cfg.set(key, value)
+#
+#     def load(self):
+#         return self.application
 
 
 @asynccontextmanager
@@ -42,14 +42,14 @@ async def lifespan(app: FastAPI):
         await conn.run_sync(Base.metadata.create_all)
     yield
 
+app = FastAPI(title="Trust Not Trust", lifespan=lifespan)
+app.include_router(auth_router)
+app.include_router(rate_router)
+app.include_router(search_router)
+app.include_router(user_router)
+
 
 if __name__ == "__main__":
-    app = FastAPI(title="Trust Not Trust", lifespan=lifespan)
-    app.include_router(auth_router)
-    app.include_router(rate_router)
-    app.include_router(search_router)
-    app.include_router(user_router)
-
-    custom_gunicorn_app = CustomGunicornApp(app)
-    custom_gunicorn_app.run()
-    # uvicorn.run("main:app", reload=True)
+    # custom_gunicorn_app = CustomGunicornApp(app)
+    # custom_gunicorn_app.run()
+    uvicorn.run("main:app", reload=True)
